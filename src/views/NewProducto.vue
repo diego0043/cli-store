72d9868b6c3f6944c654e7f8a7d25a90e12c34b2
diego0043 <ve18007@ues.edu.sv>
2021-11-04 23:11:39 -0600

    nuevo producto al 5%

--- src/views/NewProducto.vue
+++ src/views/NewProducto.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <navbar></navbar>
+    <new-producto></new-producto>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import NewProducto from '../components/NewProducto.vue'
 export default {
   name: 'NewProduct',
   
   components: {
-    Navbar
+    Navbar,
+    NewProducto
   }
 
 }
